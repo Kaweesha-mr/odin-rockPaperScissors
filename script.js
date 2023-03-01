27eb175d55a5991afcfb8aeaa46efe4adc27a37d
Kaweesha-mr <kaweesha.mr@gmail.com>
2023-03-01 16:40:13 +0530

    code completed

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 function playRound(playerSelection, computerSelection) {
 
-    if(playerSelection == "rock" && computerSelection == "rock"){
-
-    }
-    else if(playerSelection == "paper" && computerSelection == "paper"){
-        
-    }
-    else if(playerSelection == "scissors" && computerSelection == "scissors"){
-        
-    }
-    else if(playerSelection == "scissors" && computerSelection == "paper"){
+    if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You won!, Scissors cuts the paper";
         
     }
     else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "you lose!, rock breaks the scissors";
         
     }
-    else if(playerSelection == "scissors" && computerSelection == "scissors"){
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "you lose!, Scissors cuts the paper";
         
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "you won!, paper covers the rock";
+
+        
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "you won!, rock breaks the scissors";
+        
+    }
+    else if(playerSelection == "rock" && computerSelection == "paper"){
+        return "you lose!, paper covers the rock";
+        
+    }
+    else{
+        return "tie,tie"
     }
   }
+
+
 
 function getComputerChoice(){
 
     const array = ["rock","paper","scisors"];
     
     const randomIndex = Math.floor(Math.random()*array.length);
-
-    return array[randomIndex].toLowerCase;
+    console.log(randomIndex);
+    return array[randomIndex];  
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice:");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+     }
+
+}
+
+game();
